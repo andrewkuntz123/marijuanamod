8ff412539c97e6ccbed992b2f6eca9058c4199ba
unknown <andrew.kuntz1@yahoo.com>
2025-02-14 17:50:02 -0600

    Update

--- src/main/java/net/andrewk/marijuana/item/ModItems.java
+++ src/main/java/net/andrewk/marijuana/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.andrewk.marijuana.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.andrewk.marijuana.TutorialMod;
@@ -9,7 +8,6 @@ import net.andrewk.marijuana.block.ModBlocks;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
